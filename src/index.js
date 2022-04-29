1c770d6865918067c8c897bfbcab3a02a0c9ed62
Elstan Lewis <elstanlewis7@gmail.com>
2022-04-28 22:08:45 -0400

    added details page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import App from './App';
 import store from './app/store';
 import 'antd/dist/antd.css';
-import 'antd/dist/antd.less';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
