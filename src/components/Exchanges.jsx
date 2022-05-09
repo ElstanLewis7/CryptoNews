f6a3d8c83619f03c4f0f534b039c05db701f7b27
Elstan Lewis <elstanlewis7@gmail.com>
2022-05-08 22:16:37 -0400

    Deployed

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import { Collapse, Row, Col, Typography, Avatar, Select } from 'antd';
 import HTMLReactParser from 'html-react-parser';
 
 import { useGetExchangeRateQuery } from '../services/exchangeRateApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
 import Loader from './Loader';
 
 const { Text } = Typography;
 const { Panel } = Collapse;
+const { Option } = Select;
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangeRateQuery();
+  const [cryptocurrency, setCryptocurrency] = useState('Cryptocurrency');
+
+  const { data: exchangeRate, isFetching } =
+    useGetExchangeRateQuery(cryptocurrency);
+  const { data } = useGetCryptosQuery(100);
+
   const exchangesList = data?.data?.exchanges;
 
   if (isFetching) return <Loader />;
@@ -18,7 +26,27 @@ const Exchanges = () => {
   return (
     <>
       <Row>
-        <Col span={6}>CryptoCurrency</Col>
+        <Col span={6}>
+          <Col span={24}>
+            <Select
+              showSearch
+              className="select-news"
+              placeholder="Select a Crypto"
+              optionFilterProp="children"
+              onChange={(value) => setCryptocurrency(value)}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase())
+              }
+            >
+              <Option value="Cryptocurrency">Cryptocurrency</Option>
+              {data?.data?.coins.map((coin, i) => (
+                <Option value={coin.name} key={i}>
+                  {coin.name}
+                </Option>
+              ))}
+            </Select>
+          </Col>
+        </Col>
         <Col span={6}>US Dollar Exchange Rate</Col>
         <Col span={6}></Col>
         <Col span={6}>Change</Col>
